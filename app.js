a0af6ce7407abc755d45d9919e20900aa0fa620d
LogicalTrue <59065051+LogicalTrue@users.noreply.github.com>
2022-12-06 20:35:44 -0300

    Editando archivo app.js, con la modalidad del index.js que utilizaba

--- app.js
+++ app.js
@@ -1,46 +1,22 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+//Utilizaremos express
+const express = require('express');
+const app = express();
 
-var indexRouter = require('./routes/index');
+//Para no tener problemas con utf-8
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }))
+
+//En este caso utilizaremos el puerto 3006
+
+app.listen(3006);
 
 const {Cabina} = require('./db/models');
 
-app.use('/', indexRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 //listar todos los peaje
 
-app.get('listar-peajes', async function(req, res){
+app.get('/listar-peajes', async function(req, res){
 
   // Cada peaje debe incluir la dirección donde se encuentra. La dirección debe especificar
   // provincia, número de ruta, y kilómetro. No es necesario usar datos geográficos reales,
@@ -49,7 +25,6 @@ app.get('listar-peajes', async function(req, res){
   let obtenerVista = await Cabina.findAll({})
 
   res.send(obtenerVista);
-
 
 })
 
