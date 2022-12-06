7fae82d912eb45e3e54956611c98a73e76d9e0d7
LogicalTrue <59065051+LogicalTrue@users.noreply.github.com>
2022-12-06 19:49:52 -0300

    Issue peajes-listar

--- config/database.js
+++ config/database.js
@@ -1,9 +1,14 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+module.exports =
 {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "examen_final_2022_12",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
     "dialect": "mysql"
   },
   "test": {
