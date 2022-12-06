874d70fa5edef800c40135fc1fd8ee8d7b167427
augusto1812 <augusto.8121@gmail.com>
2022-12-06 20:56:48 -0300

    creade get de peajes

--- routes/peajes.js
+++ routes/peajes.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var peajes = require("../src/repositories/peajes")
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.json({});
+router.get('/', async function(req, res, next) {
+  res.json(await peajes.getAll());
 });
 
 module.exports = router;
