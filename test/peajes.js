c692a8aac45de99cf13208a75727a922e5b2047d
IEzequiel <iaezequiel@gmail.com>
2022-12-06 21:48:26 -0300

    se crean los test

---
+++ test/peajes.js
@@ -0,0 +1,52 @@
+const { assert } = require('chai')
+const request = require('supertest')
+const app = require('../app')
+const { Peajes } = require ('../db/models')
+
+describe ('Peajes', function() {
+
+    describe('Listado de peajes', function () {
+
+        // before(async function () {
+        //     await Peajes.create({
+        //         provincia: 'Buenos Aires',
+        //         ruta: 2,
+        //         kilometro: 30,
+        //         createdAt: '2022-12-6',
+        //         updatedAt: '2022-12-6'
+        //     })
+
+        //     await Peajes.create({
+        //         provincia: 'Cordoba',
+        //         ruta: 5,
+        //         kilometro: 50,
+        //         createdAt: '2022-12-6',
+        //         updatedAt: '2022-12-6'
+        //     })
+        // })
+
+        it.skip('Conocer cuantos peajes cargue en la base de datos', function (done) {
+            request(app)
+            .get('/peajes')
+            .expect(200)
+            .end(function (res,err) {
+                assert.equal(res.body.length,'2')
+                if(err) return done(err);
+                return done();
+            })
+
+            it('Que incluya la direccion como objeto anidado', function (done) {
+                request(app)
+                .get('/peajes')
+                .expect(200)
+                .end(function (res,err) {
+                    assert.equal(res.body[0].provincia,'Buenos Aires')
+                    assert.equal(res.body[0].provincia,'Buenos Aires')
+                    if(err) return done(err);
+                    return done();
+                })
+
+        })
+    })
+
+})
