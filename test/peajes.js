264ca52b3103526adc7336053451f5788cda290d
augusto1812 <augusto.8121@gmail.com>
2022-12-06 21:21:07 -0300

    agregados 2 test

--- test/peajes.js
+++ test/peajes.js
@@ -1,5 +1,44 @@
 const { assert } = require("chai");
-const request = requiere ("supertest");
-const app = require ("../app");
-const { Peaje }  = require("../db/models");
+const request = require("supertest");
+const app = require("../app");
+const { Peaje } = require("../db/models");
+const { Direccion } = require("../db/models");
 
+describe("Testeo de peajes", function () {
+  var direccionPrueba;
+
+  before(async function () {
+    direccionPrueba = await Direccion.create({
+      provincia: "Santa Fe",
+      ruta: 10,
+      kilometro: 28,
+    });
+  });
+
+  after(async function() {
+    await direccionPrueba.destroy();
+  })
+  describe("Testeo del GET", function () {
+    it("Obtener todos los peajes", function () {
+      request(app)
+        .get("/peajes")
+        .set("Accept", "application(json")
+        .expect("Content-Type", /json/)
+        .expect(200)
+        .end(function (err, res) {
+          if (err) throw err;
+        });
+    });
+    it("Obtener evento por direccion", function () {
+      request(app)
+        .get("/peajes")
+        .query({ direccionId: direccionPrueba.id })
+        .set("Accept", "application(json")
+        .expect("Content-Type", /json/)
+        .expect(200)
+        .end(function (err, res) {
+          if (err) throw err;
+        });
+    });
+  });
+});
